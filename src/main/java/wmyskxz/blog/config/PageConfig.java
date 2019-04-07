38fafebdbab391b8ea8b71f53094ff9f0819876b
wmyskxz <939419061@qq.com>
2019-04-07 16:34:56 +0800

    分页配置类

---
+++ src/main/java/wmyskxz/blog/config/PageConfig.java
@@ -0,0 +1,15 @@
+package wmyskxz.blog.config;
+
+/**
+ * 分页相关配置
+ *
+ * @auth:wmyskxz
+ * @date:2019/03/15 - 13:16
+ */
+public class PageConfig {
+
+    /** 默认从0页开始 */
+    public static final String PAGE_NUM = "0";
+    /** 默认分页大小 */
+    public static final String PAGE_SIZE = "10";
+}
